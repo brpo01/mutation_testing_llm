abeb804d14b3ce28afa0a560659c7a54a6015b09
brpo01 <opraise139@gmail.com>
2024-09-07 12:16:37 +0100

    added client folder

---
+++ client-v2/src/app/services/chat.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatService {
+
+  constructor(private apiService: ApiService) { 
+  }
+
+  startChat(data:any, id: String) {
+    return this.apiService.post(`chat/${id}`, data);
+  }
+  getChataHistory(id:String) {
+    return this.apiService.get(`chat/${id}`)
+  }
+}
